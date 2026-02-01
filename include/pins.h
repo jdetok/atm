450bec817a7b1055cd1085c38fb53eb0a2b51476
jdetok <jdeko17@gmail.com>
2026-02-01 15:35:34 -0600

    setup and onoff funcs, still only blink

---
+++ include/pins.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define D2 (1 << PD2)
+#define D3 (1 << PD3)
+#define D4 (1 << PD4)
+#define D5 (1 << PD5)
+
+#define D13 (1 << PB5)
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include "pins.h"
 
-#define IO13 (1<< PB5)
+#define DT 500
+
+void setup_out_pins() {
+    DDRB |= D13;
+    DDRD |= D2 | D3 | D4 | D5;
+}
+
+void pin_onoff(int pin, volatile uint8_t* port, bool on) {
+    if (on) {
+        *port |= pin;
+    } else {
+        *port &= ~pin;
+    }
+}
 
 int main() {
-    DDRD |= IO13;
+    setup_out_pins();
 
     while (1) {
-        PORTB |= IO13;
-        _delay_ms(500);
+        pin_onoff(D13, &PORTB, 1); 
+        _delay_ms(DT);
 
-        PORTB &= ~IO13;
-        _delay_ms(500);
+        pin_onoff(D13, &PORTB, 0); 
+        _delay_ms(DT);
     }
 }
